ad13211dacc5c3df85122818f7627a2ebcd4dc00
Poliana Brandão <146744661+polianabrandao@users.noreply.github.com>
2023-10-08 22:37:53 -0300

    Atualização

--- ex05.py
+++ ex05.py
@@ -1,13 +1,11 @@
 a = []
-x = []
-m = 0
 for y in range(10):
     a.append(int(input('Digite um número: ')))
 
-x.append(int(input('Digite um número para multiplicar: ')))
+x = int(input('Digite um número para multiplicar: '))
 print(a)
 print(x)
 
 for y in a:
     m = a[y] * x
-    print(m)
+    print(f'{x} x {a[y]} = {m}')
